6b75b3b32ee95e1b57a93dcfcdf2f0f89ccf7d10
Vadim Sokoltsov <vforvad@gmail.com>
2020-05-31 23:54:18 +0300

    Remove model

--- app/models/beeg_event.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// BeegEvent represents event structure
-type BeegEvent struct {
-	ID    int    `db:"id" json:"id"`
-	Label string `db:"label" json:"label"`
-	Count int    `db:"count" json:"count"`
-}
